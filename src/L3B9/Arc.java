fefe3b8874be01a51c60c42b22567b9efb2fd952
R4KO <rakotovahiny@hotmai.fr>
2020-04-21 03:35:24 +0200

    Can read

---
+++ src/L3B9/Arc.java
@@ -0,0 +1,13 @@
+package L3B9;
+
+public class Arc {
+    private int origine;
+    private int destination;
+    private int valeur;
+
+    public Arc(int origine, int destination, int valeur) {
+        this.origine = origine;
+        this.destination = destination;
+        this.valeur = valeur;
+    }
+}
